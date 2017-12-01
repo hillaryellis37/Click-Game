00eed91402e53b87ede8d1f71842b763b859dda9
Live Sound Systems <hillaryellis37@gmail.com>
2017-11-30 20:01:14 -0800

    added css and bootstrap styling

--- clickgame/src/components/CharContainer/CharContainer.js
+++ clickgame/src/components/CharContainer/CharContainer.js
@@ -1,6 +1,11 @@
 import React from "react";
 import "./CharContainer.css";
 
-const CharContainer = props => <div className="char-container">{props.children}</div>;
-
+const CharContainer = props => (
+	<div className="row d-flex justify-content-center">
+		<div className="col-md-7">
+			<div className="char-container">{props.children}</div>;
+		</div>
+	</div>
+);
 export default CharContainer;
